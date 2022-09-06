f52f68131f2346d193a61896ec5b5568feaf4109
Petr <strexet@gmail.com>
2022-09-06 14:38:30 +0700

    Update SceneShortcutUtil.cs

--- Editor/SceneShortcutUtil.cs
+++ Editor/SceneShortcutUtil.cs
@@ -2,47 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 #if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
+
 #else
 using UnityEngine.Experimental.UIElements;
 #endif
 
 namespace UsefulTools.Editor
 {
-	[InitializeOnLoad]
-	public static class SceneShortcutUtil
-	{
-		private static ScriptableObject _toolbar;
-		private static string[] _scenePaths;
-		private static string[] _sceneNames;
+    [InitializeOnLoad]
+    public static class SceneShortcutUtil
+    {
+        private static ScriptableObject _toolbar;
+        private static string[] _scenePaths;
+        private static string[] _sceneNames;
 
-		static SceneShortcutUtil()
-		{
-			EditorApplication.update -= Update;
-			EditorApplication.update += Update;
-		}
+        static SceneShortcutUtil()
+        {
+            EditorApplication.update -= Update;
+            EditorApplication.update += Update;
+        }
 
-		private static void Update()
-		{
-			if (_toolbar == null)
-			{
-				var editorAssembly = typeof(UnityEditor.Editor).Assembly;
+        private static void Update()
+        {
+            if (_toolbar == null)
+            {
+                var editorAssembly = typeof(UnityEditor.Editor).Assembly;
 
-				var toolbars = UnityEngine.Resources.FindObjectsOfTypeAll(editorAssembly.GetType("UnityEditor.Toolbar"));
-				_toolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
-				if (_toolbar != null)
-				{
+                var toolbars = Resources.FindObjectsOfTypeAll(editorAssembly.GetType("UnityEditor.Toolbar"));
+                _toolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
+
+                if (_toolbar != null)
+                {
 #if UNITY_2020_1_OR_NEWER
-				var windowBackendPropertyInfo =
- editorAssembly.GetType("UnityEditor.GUIView").GetProperty("windowBackend", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-				var windowBackend = windowBackendPropertyInfo.GetValue(_toolbar);
-				var visualTreePropertyInfo = windowBackend.GetType().GetProperty("visualTree", BindingFlags.Public| BindingFlags.Instance);
-				var visualTree = (VisualElement)visualTreePropertyInfo.GetValue(windowBackend);
+                    var windowBackendPropertyInfo =
+                        editorAssembly.GetType("UnityEditor.GUIView").GetProperty("windowBackend",
+                            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    object windowBackend = windowBackendPropertyInfo.GetValue(_toolbar);
+                    var visualTreePropertyInfo =
+                        windowBackend.GetType().GetProperty("visualTree", BindingFlags.Public | BindingFlags.Instance);
+
+                    var visualTree = (VisualElement)visualTreePropertyInfo.GetValue(windowBackend);
 #else
 					PropertyInfo visualTreePropertyInfo = editorAssembly.GetType("UnityEditor.GUIView")
 					   .GetProperty("visualTree", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
@@ -50,71 +56,76 @@ namespace UsefulTools.Editor
 					VisualElement visualTree = (VisualElement)visualTreePropertyInfo.GetValue(_toolbar, null);
 #endif
 
-					var container = (IMGUIContainer)visualTree[0];
+                    var container = (IMGUIContainer)visualTree[0];
 
-					var onGUIHandlerFieldInfo = typeof(IMGUIContainer).GetField("m_OnGUIHandler",
-						BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    var onGUIHandlerFieldInfo = typeof(IMGUIContainer).GetField("m_OnGUIHandler",
+                        BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
-					var handler = (Action)onGUIHandlerFieldInfo.GetValue(container);
-					handler -= OnGUI;
-					handler += OnGUI;
-					onGUIHandlerFieldInfo.SetValue(container, handler);
-				}
-			}
+                    var handler = (Action)onGUIHandlerFieldInfo.GetValue(container);
+                    handler -= OnGUI;
+                    handler += OnGUI;
+                    onGUIHandlerFieldInfo.SetValue(container, handler);
+                }
+            }
 
-			if (_scenePaths == null || _scenePaths.Length != EditorBuildSettings.scenes.Length)
-			{
-				var scenePaths = new List<string>();
-				var sceneNames = new List<string>();
+            if (_scenePaths == null || _scenePaths.Length != EditorBuildSettings.scenes.Length)
+            {
+                var scenePaths = new List<string>();
+                var sceneNames = new List<string>();
 
-				foreach (var scene in EditorBuildSettings.scenes)
-				{
-					if (scene.path == null || scene.path.StartsWith("Assets") == false)
-						continue;
+                foreach (var scene in EditorBuildSettings.scenes)
+                {
+                    if (scene.path == null || scene.path.StartsWith("Assets") == false)
+                    {
+                        continue;
+                    }
 
-					var scenePath = Application.dataPath + scene.path.Substring(6);
+                    string scenePath = Application.dataPath + scene.path.Substring(6);
 
-					scenePaths.Add(scenePath);
-					sceneNames.Add(Path.GetFileNameWithoutExtension(scenePath));
-				}
+                    scenePaths.Add(scenePath);
+                    sceneNames.Add(Path.GetFileNameWithoutExtension(scenePath));
+                }
 
-				_scenePaths = scenePaths.ToArray();
-				_sceneNames = sceneNames.ToArray();
-			}
-		}
+                _scenePaths = scenePaths.ToArray();
+                _sceneNames = sceneNames.ToArray();
 
-		private static void OnGUI()
-		{
-			using (new EditorGUI.DisabledScope(Application.isPlaying))
-			{
-				var rect = new Rect(0, 0, Screen.width, Screen.height)
-				{
-					xMin = EditorGUIUtility.currentViewWidth * 0.5f + 100.0f,
-					xMax = EditorGUIUtility.currentViewWidth - 350.0f,
-					y = 8.0f,
-				};
+                Debug.Log($"[DEBUG]<color=green>{nameof(SceneShortcutUtil)}.{nameof(Update)}></color> " +
+                          $"Got scene paths and names: {_sceneNames.Length} names and {_scenePaths.Length} paths");
+            }
+        }
 
-				using (new GUILayout.AreaScope(rect))
-				{
-					var sceneName = SceneManager.GetActiveScene().name;
-					var sceneIndex = -1;
+        private static void OnGUI()
+        {
+            using (new EditorGUI.DisabledScope(Application.isPlaying))
+            {
+                var rect = new Rect(0, 0, Screen.width, Screen.height)
+                {
+                    xMin = EditorGUIUtility.currentViewWidth * 0.5f + 100.0f,
+                    xMax = EditorGUIUtility.currentViewWidth - 350.0f,
+                    y = 8.0f
+                };
 
-					for (var i = 0; i < _sceneNames.Length; ++i)
-					{
-						if (sceneName == _sceneNames[i])
-						{
-							sceneIndex = i;
-							break;
-						}
-					}
+                using (new GUILayout.AreaScope(rect))
+                {
+                    string sceneName = SceneManager.GetActiveScene().name;
+                    int sceneIndex = -1;
 
-					var newSceneIndex = EditorGUILayout.Popup(sceneIndex, _sceneNames, GUILayout.Width(200.0f));
-					if (newSceneIndex != sceneIndex)
-					{
-						EditorSceneManager.OpenScene(_scenePaths[newSceneIndex], OpenSceneMode.Single);
-					}
-				}
-			}
-		}
-	}
+                    for (int i = 0; i < _sceneNames.Length; ++i)
+                    {
+                        if (sceneName == _sceneNames[i])
+                        {
+                            sceneIndex = i;
+                            break;
+                        }
+                    }
+
+                    int newSceneIndex = EditorGUILayout.Popup(sceneIndex, _sceneNames, GUILayout.Width(200.0f));
+                    if (newSceneIndex != sceneIndex)
+                    {
+                        EditorSceneManager.OpenScene(_scenePaths[newSceneIndex], OpenSceneMode.Single);
+                    }
+                }
+            }
+        }
+    }
 }
