e6afb895102ef9eec6a68b35cebf29f9e59c10fa
Petr Strekhletov <strexet@gmail.com>
2022-10-20 13:50:39 +0700

    Added obesrvable variable

---
+++ Runtime/DataStructures/ObservableVariable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace UsefulTools.Runtime.DataStructures
+{
+    [Serializable]
+    public class ObservableVariable<T>
+    {
+        [SerializeField] private T _value;
+
+        public event Action<T, T> OnValueChanged;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                var previous = _value;
+                _value = value;
+                OnValueChanged?.Invoke(previous, _value);
+            }
+        }
+
+        public ObservableVariable(T @default) => _value = @default;
+
+        public ObservableVariable() : this(default) { }
+    }
+}
