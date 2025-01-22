fd3206eff4ef80958870c30cc9c6756fe768c1c0
Petr Strekhletov <strexet@gmail.com>
2025-01-22 17:19:15 +0700

    Update IInputReader.cs

--- Runtime/Input/IInputReader.cs
+++ Runtime/Input/IInputReader.cs
@@ -1,22 +1,27 @@
 namespace StrexetGames.UsefulTools.Runtime.Input
 {
 	/// <summary>
-	/// Input reader interface for Unity Input package.
-	/// It is convenient to inherit from this interface and ScriptableObject to create a custom input reader.
-	/// Create an inputActions field to store input actions in the custom input reader implementation.
-	/// Implement creation and enable of input actions in EnablePlayerActions method.
+	///     Input reader interface for Unity Input package.
+	///     It is convenient to inherit from this interface and ScriptableObject to create a custom input reader.
+	///     Create an inputActions field to store input actions in the custom input reader implementation.
+	///     Implement creation and enable/disable of input actions in EnableInputActions/DisableInputActions methods.
 	/// </summary>
 	public interface IInputReader
 	{
 		/// <summary>
-		/// Default implementation of this method would be:
-		/// 
-		/// if (inputActions == null) {
-		///		inputActions = new SomeInputActions();
-		///		inputActions.SomeInputReceiver.SetCallbacks(this);
-		/// }
-		/// inputActions.Enable();
+		///     Default implementation of this method would be:
+		///			if (inputActions == null) {
+		///				inputActions = new SomeInputActions();
+		///				inputActions.SomeInputReceiver.SetCallbacks(this);
+		///			}
+		///     inputActions.Enable();
 		/// </summary>
-		void EnablePlayerActions();
+		void EnableInputActions();
+
+		/// <summary>
+		///     Default implementation of this method would be:
+		///			inputActions?.Disable();
+		/// </summary>
+		void DisableInputActions();
 	}
 }
