c036e951d7cd88aabcbd093f2bc46cd93e4cee6c
Petr Strekhletov <strexet@gmail.com>
2025-02-28 18:44:52 +0700

    Removed odin references

--- Editor/ForRuntime/Floats/FloatMinMaxClassDrawer.cs
+++ Editor/ForRuntime/Floats/FloatMinMaxClassDrawer.cs
@@ -1,34 +1,21 @@
-using Sirenix.OdinInspector.Editor;
-using Sirenix.Utilities;
 using UnityEditor;
 using UnityEngine;
 using UsefulTools.Editor;
 
 namespace UsefulTools.Runtime.DataStructures.Floats.Editor
 {
-    public class FloatMinMaxClassDrawer : OdinValueDrawer<FloatMinMax>
+    public class FloatMinMaxClassDrawer : PropertyDrawer
     {
-        protected override void DrawPropertyLayout(GUIContent label)
+        private const string MinPropertyName = nameof(FloatMinMax.min);
+        private const string MaxPropertyName = nameof(FloatMinMax.max);
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            FloatMinMax value = ValueEntry.SmartValue;
+            var minProp = property.FindPropertyRelative(MinPropertyName);
+            var maxProp = property.FindPropertyRelative(MaxPropertyName);
 
-            var rect = EditorGUILayout.GetControlRect();
-
-            // In Odin, labels are optional and can be null, so we have to account for that.
-            if (label != null)
-            {
-                rect = EditorGUI.PrefixLabel(rect, label);
-            }
-
-            var prev = EditorGUIUtility.labelWidth;
-            EditorGUIUtility.labelWidth = 20;
-
-            value.min = EditorGUI.FloatField(rect.AlignLeft(rect.width * 0.5f), "Min", value.min);
-            value.max = EditorGUI.FloatField(rect.AlignRight(rect.width * 0.5f), "Max", value.max);
-
-            EditorGUIUtility.labelWidth = prev;
-
-            ValueEntry.SmartValue = value;
+            EditorUIHelper.DrawPropertiesInLine(position, minProp, maxProp);
         }
+    }
     }
 }
